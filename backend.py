03507e9321c80f984fb1a14955019a6d3653a983
jams-ctrl <348787888@gapps.yrdsb.ca>
2026-07-15 16:57:50 -0400

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+
+from storage import get_conn, init_db, edgar_summary, stocktwits_summary
+
+app = Flask(__name__)
+
+# if just loading up the page
+@app.route("/")
+def index():
+    return render_template("stockSearcher.html")
+
+# if ticker is entered as search
+@app.route("/ticker_given")
+def summary():
+    ticker = request.args.get("ticker", "").strip().uppder()
+
+    if not ticker:
+        return jsonify ({"error": "missing ticker"}), 400
+    
+    with get_conn as conn:
+        edgar = edgar_summary(conn, ticker)
+        stocktwits = stocktwits_summary(conn, ticker)
+
+    return jsonify ({"ticker": ticker, "edgar": edgar, "stocktwits": stocktwits})
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True, port=8080)
